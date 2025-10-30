89ec2626f38d39be66de2d4de420cf15e292fe16
franck <carnet.franck.paul@gmail.com>
2025-10-30 14:39:58 +0100

    New release 1.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Custom HTML Element',
     'Custom HTML Element for Dotclear server',
     'Franck Paul',
-    '1.0',
+    '1.1',
     [
-        'date'        => '2025-10-30T14:38:33+0100',
+        'date'        => '2025-10-30T14:38:33+01.1',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
